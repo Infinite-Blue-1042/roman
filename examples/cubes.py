6629fb8ceed42b493707b5cb1ff9381a73051a87
Siddharth Gupta <81249728+Infinite-Blue-1042@users.noreply.github.com>
2021-03-28 09:26:14 -0700

    Update cubes.py

--- examples/cubes.py
+++ examples/cubes.py
@@ -1,11 +1,8 @@
-# this deals with the case when RoMan is not installed as a package
-import os
+from roman import *
+import math, time, os
+
 rootdir = os.path.dirname(os.path.dirname(__file__))
 os.sys.path.insert(0, rootdir)
-
-from roman import *
-import math
-import time
 
 cubePoses = [[-0.15,-0.3,0.025], [-0.35,-0.1,0.025], [-0.55,-0.3,0.025]]
 cubeSize =0.05
@@ -15,7 +12,6 @@ def setup_sim(simenv):
     simenv.make_box([cubeSize]*3, cubePoses[2], color=(0.2,0.2,0.8,1), mass = 0.1)
 
 if __name__ == '__main__':
-    # start simulation
     robot = connect(use_sim=True, sim_init=setup_sim)
     home_pose = robot.arm.state.tool_pose().clone()
     stack_pose = home_pose.clone()
@@ -23,16 +19,15 @@ if __name__ == '__main__':
     robot.hand.move(128)
     robot.hand.set_mode(hand.GraspMode.PINCH)
     
-    # stack the cubes
-    for i in range(3):
+    for i in range(0, 3, 1):
         pose = home_pose.clone()
         pose[:3] = cubePoses[i]
-        pose[2] = 0.1 # pre-grasp
+        pose[2] = 0.1
         robot.arm.move(pose, max_speed=2, max_acc=1)
         pose[2] = cubeSize - grasp_depth
         robot.arm.move(pose)
         robot.hand.close()
-        pose[2] = 0.1 # pre-grasp
+        pose[2] = 0.1
         robot.arm.move(pose)
         robot.arm.move(home_pose, max_speed=1, max_acc=0.2)
         stack_pose[2] = (i+1) * cubeSize - grasp_depth
